40e9fb744d02a24ba13487825e9c0cb7ce21fd9e
bulsu-emagustin <2024100668@ms.bulsu.edu.ph>
2026-03-31 02:00:30 +0800

    Initial commit

--- src/EDSxADS_Final_Project/RecyclingCollectionMonitoringSystem.java
+++ src/EDSxADS_Final_Project/RecyclingCollectionMonitoringSystem.java
@@ -1,7 +1,9 @@
 package EDSxADS_Final_Project;
 
 //Imports
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
 import javax.swing.ImageIcon;
@@ -17,11 +19,10 @@ import javax.swing.JTextField;
 
 public class RecyclingCollectionMonitoringSystem {
 
-    //Main Method
+    //Main Method (DIto palagi magrun ng code)
     public static void main(String[] args) {
-        new UniversityRecycleZone();
+        new WelcomeFrame();
     }
-
 }
 
 //Main Frame
@@ -29,71 +30,91 @@ class UniversityRecycleZone extends JFrame {
 
     //Variables
     JFrame MainF;
-    JPanel header, leftPanel;
+    JPanel header, UnivImageP, leftPanel, UserImageP, MainP, ColumnChart;
     JDialog Login, Contribution, History;
-    JLabel UniversityL, WelcomeL, UserL, AdminL, LoginL, UsernameL, PasswordL, ContributionL, SIDL, MTypeL, QuantityL;
+    JLabel UnivIcon, UserIcon, UniversityL, UserL, AdminL, LoginL, UsernameL, PasswordL, ContributionL, SIDL, MTypeL, QuantityL, StudentCL;
     JButton AdminButton, AddContriButton, ViewContriButton, ClearButton, EnterButton, CancelButton, SearchButton;
     JTextField Usernamefield, Passworfield, IDfield, Quantityfield;
     JComboBox<String> MTypeBox;
     JTable Table;
-    JScrollPane Scroll;
+    JScrollPane TableS;
+    ImageIcon MainBack, UnivL, UserIconL, ScaledUnivIcon, ScaledUserIcon;
+    Image Logo, Logo2;
 
     public UniversityRecycleZone() {
 
         //Containers
         MainF = new JFrame("University Recycle Zone");
         header = new JPanel();
+        leftPanel = new JPanel();
+        UnivImageP = new JPanel();
+        UserImageP = new JPanel();
         Login = new JDialog(this, "Admin Login", true);
         Contribution = new JDialog(this, "Contribution Form", true);
         History = new JDialog(this, "User History", true);
         String[] Mtype = {"Plastic", "Glass", "Paper", "Metal", "E-Waste"};
-        String[] Atributes = {"Student ID", "Department", "Material Type", "Quantity", "Date"};
+        String[] Atributes = {"Student ID", "Department", "Material Type", "Quantity", "Transaction", "Date"};
         String[][] Values = {};
+        MainP = new JPanel(); 
+        ColumnChart = new WeeklyColumnChart();
 
         //Background Image
-        ImageIcon icon = new ImageIcon("Main_Background.jpg");
-        JLabel Mbackground = new JLabel(icon);
+        MainBack = new ImageIcon("Main_Background.jpg");
+        JLabel Mbackground = new JLabel(MainBack);
         Mbackground.setLayout(null);
         MainF.setContentPane(Mbackground);
 
+        //Layout Config
+        MainF.setLayout(new BorderLayout());
+
         //Header Config
-        header.setBackground(Color.green);
-        header.setBounds(0, 0, 1500, 90);
+        header.setBackground(new Color(34, 139, 34));
+        header.setPreferredSize(new Dimension(1500, 90));
         header.setLayout(null);
-        
-        //Left panel Config
-        leftPanel = new JPanel();
-        leftPanel.setBackground(Color.yellow);
-        leftPanel.setBounds(0, 0, 200, 800);
-        leftPanel.setLayout(null);
-        
-        //Headr Title config 
+        UnivImageP.setBounds(10, 10, 80, 70);
+        UnivImageP.setLayout(null);
+        UnivL = new ImageIcon("URZLogo.png");
+        Logo = UnivL.getImage().getScaledInstance(80, 70, Image.SCALE_SMOOTH);
+        ScaledUnivIcon = new ImageIcon(Logo);
+        UnivIcon = new JLabel(ScaledUnivIcon);
+        UnivIcon.setBounds(0, 0, 80, 70);
         UniversityL = new JLabel("University Recycle Zone");
-        UniversityL.setBounds(30, 20, 500, 50);
+        UniversityL.setBounds(61, 20, 500, 50);
         UniversityL.setFont(new Font("Arial Black", Font.BOLD, 30));
-        
+
         //Login button
-        AdminL = new JLabel("Admin");
+        AdminL = new JLabel("ADMIN");
         AdminL.setBounds(1240, 25, 120, 40);
-        AdminL.setFont(new Font("Arial", Font.BOLD, 30));
+        AdminL.setFont(new Font("Bodoni MT", Font.BOLD, 30));
         AdminButton = new JButton("Login");
-        AdminButton.setFont(new Font("Arial", Font.BOLD, 20));
-        AdminButton.setBounds(1340, 25, 120, 40);
+        AdminButton.setForeground(Color.red);
+        AdminButton.setFont(new Font("Arial Black", Font.BOLD, 20));
+        AdminButton.setBounds(1350, 20, 120, 50); 
         new LoginFunction();
 
-        //Title
-        WelcomeL = new JLabel("WELCOME USER");
-        WelcomeL.setBounds(450, 95, 700, 90);
-        WelcomeL.setFont(new Font("Arial Black", Font.BOLD, 70));
+        //Left panel Config
+        leftPanel.setBackground(new Color(72, 209, 204));
+        leftPanel.setPreferredSize(new Dimension(400, 800));
+        leftPanel.setLayout(null);
+        UserImageP.setBounds(50, 50, 300, 250);
+        UserImageP.setLayout(null);
+        UserIconL = new ImageIcon("UserIcon.png");
+        Logo2 = UserIconL.getImage().getScaledInstance(300, 250, Image.SCALE_SMOOTH);
+        ScaledUserIcon = new ImageIcon(Logo2);
+        UserIcon = new JLabel(ScaledUserIcon);
+        UserIcon.setBounds(0, 0, 300, 250);
+        UserL = new JLabel("USER");
+        UserL.setFont(new Font("Arial", Font.BOLD, 30));
+        UserL.setBounds(160, 310, 100, 50);
+        AddContriButton = new JButton("Add Contribution");
+        AddContriButton.setBackground(Color.LIGHT_GRAY);
+        AddContriButton.setFont(new Font("Arial", Font.BOLD, 30));
+        AddContriButton.setBounds(50, 390, 300, 100);
+        ViewContriButton = new JButton("View Contribution");
+        ViewContriButton.setBackground(Color.LIGHT_GRAY);
+        ViewContriButton.setFont(new Font("Arial", Font.BOLD, 30));
+        ViewContriButton.setBounds(50, 530, 300, 100);
 
-        //JTable
-        //Add Contribution Button
-        ImageIcon icon1 = new ImageIcon("RecycleButton.png");
-        Image img1 = icon1.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
-        ImageIcon scaledIcon1 = new ImageIcon(img1);
-        AddContriButton = new JButton(scaledIcon1);
-        AddContriButton.setBounds(50, 290, 100, 100);
-        
         //Add Contribution Dialog
         AddContriButton.addActionListener(e -> {
             Contribution.setSize(500, 350);
@@ -102,13 +123,13 @@ class UniversityRecycleZone extends JFrame {
             ContributionL = new JLabel("CONTRIBUTION");
             ContributionL.setBounds(130, 10, 300, 50);
             ContributionL.setFont(new Font("Arial Black", Font.BOLD, 25));
-            
+
             SIDL = new JLabel("School ID: ");
             SIDL.setBounds(63, 40, 70, 80);
             IDfield = new JTextField(100);
             IDfield.setBounds(130, 65, 300, 30);
             MTypeL = new JLabel("Material Type: ");
-            MTypeL.setBounds(20, 93, 100, 80);
+            MTypeL.setBounds(40, 93, 100, 80);
             MTypeBox = new JComboBox<>(Mtype);
             MTypeBox.setBounds(130, 120, 300, 30);
             QuantityL = new JLabel("Quantity: ");
@@ -141,13 +162,6 @@ class UniversityRecycleZone extends JFrame {
         });
 
         //View Contribution
-        ImageIcon icon2 = new ImageIcon("HistoryButton.png");
-        Image img2 = icon2.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
-        ImageIcon scaledIcon2 = new ImageIcon(img2);
-        ViewContriButton = new JButton(scaledIcon2);
-        ViewContriButton.setBounds(50, 450, 100, 100);
-
-        //View Contribution
         ViewContriButton.addActionListener(e -> {
             History.setSize(733, 400);
             History.setLocationRelativeTo(this);
@@ -159,34 +173,53 @@ class UniversityRecycleZone extends JFrame {
             SearchButton = new JButton("Search");
             SearchButton.setBounds(609, 13, 100, 35);
             Table = new JTable(Values, Atributes);
-            Scroll = new JScrollPane(Table);
-            Scroll.setBounds(10, 60, 700, 300);
+            Table.getTableHeader().setReorderingAllowed(false);
+            TableS = new JScrollPane(Table);
+            TableS.setBounds(10, 60, 700, 300);
 
             //Adding Components
-            History.add(Scroll);
+            History.add(TableS);
             History.add(SIDL);
             History.add(IDfield);
             History.add(SearchButton);
 
             History.setVisible(true);
         });
+        
+        //Main Panel
+        MainP.setBackground(Color.WHITE);
+        MainP.setPreferredSize(new Dimension(1, 1));
+        MainP.setLayout(null);    
+        StudentCL = new JLabel("Students Weekly");
+        StudentCL.setFont(new Font("Arial Black", Font.BOLD, 60));
+        StudentCL.setBounds(270, 20, 700, 80);
+        ContributionL = new JLabel("Contribution");
+        ContributionL.setFont(new Font("Arial Black", Font.BOLD, 60));
+        ContributionL.setBounds(350, 100, 700, 80);
+        ColumnChart.setBounds(30, 200, 1030, 460);
 
         //Adding Components
-        MainF.add(header);
+        MainF.add(header, BorderLayout.NORTH);
+        header.add(UnivImageP);
+        UnivImageP.add(UnivIcon);
         header.add(UniversityL);
         header.add(AdminL);
         header.add(AdminButton);
-        MainF.add(WelcomeL);
-        
-        MainF.add(leftPanel);
+        MainF.add(leftPanel, BorderLayout.WEST);
+        leftPanel.add(UserImageP);
+        UserImageP.add(UserIcon);
         leftPanel.add(AddContriButton);
+        leftPanel.add(UserL);
         leftPanel.add(ViewContriButton);
+        MainF.add(MainP, BorderLayout.CENTER);
+        MainP.add(StudentCL);
+        MainP.add(ContributionL);
+        MainP.add(ColumnChart);
 
         //Frame Settings
         MainF.setSize(1500, 800);
         MainF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         MainF.setLocationRelativeTo(null);
-        MainF.setLayout(null);
         MainF.setVisible(true);
     }
 
@@ -210,7 +243,7 @@ class UniversityRecycleZone extends JFrame {
                 UsernameL.setBounds(50, 60, 100, 25);
                 Login.add(UsernameL);
                 Usernamefield = new JTextField();
-                Usernamefield.setBounds(150, 60, 250, 30);
+                Usernamefield.setBounds(115, 60, 285, 30);
                 Login.add(Usernamefield);
 
                 //Clear Username Button
@@ -225,7 +258,7 @@ class UniversityRecycleZone extends JFrame {
 
                 //Password Field
                 Passworfield = new JTextField();
-                Passworfield.setBounds(150, 110, 250, 30);
+                Passworfield.setBounds(115, 110, 285, 30);
                 Login.add(Passworfield);
 
                 //Clear Password Button
